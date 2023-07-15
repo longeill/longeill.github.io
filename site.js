d4ce8f27347ac941fa4719617fe73a659a4878b9
Joaquín Balogna <30188421+longeill@users.noreply.github.com>
2023-07-15 17:47:17 -0400

    Update site.js
    changed appearance of testing text

--- site.js
+++ site.js
@@ -1,1 +1,1 @@
-document.getElementById('home-top-left').innerHTML = "<p class='p-3 mono mx-auto' style='background-color:white;color:black;'>site.js is working.</p>";
+document.getElementById('home-top-left').innerHTML = "<p class='mono p-1 m-3 text-center' style='background-color:white;color:black;'>site.js is working.</p>";
