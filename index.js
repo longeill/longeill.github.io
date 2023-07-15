2402875f9ab5b50daae3200d2b94921cb070847c
Joaquín Balogna <30188421+longeill@users.noreply.github.com>
2023-07-15 17:48:58 -0400

    Update index.js
    changed appearance of testing text

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-document.getElementById('home-top-right').innerHTML = "<p class='p-3 mono mx-auto' style='background-color:white;color:black;'>index.js is working.</p>"
+document.getElementById('home-top-right').innerHTML = "<p class='mono p-1 m-3 text-center' style='background-color:white;color:black;'>index.js is working.</p>"
