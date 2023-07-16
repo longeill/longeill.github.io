783e32393723aef364881d744eca7e045e467567
Joaquín Balogna <30188421+longeill@users.noreply.github.com>
2023-07-16 15:05:07 -0400

    Create light-switch.js

---
+++ Projects/light-switch.js
@@ -0,0 +1,7 @@
+	function lightSwitch{
+		if (document.getElementById('bulb').src == 'pic_bulbon.gif') {
+	      document.getElementById("bulb").src = "pic_bulboff.gif";
+	  	} else {
+	      document.getElementById("bulb").src = "pic_bulbon.gif";
+		}
+	}
