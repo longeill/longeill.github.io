17916316b3bfe4d1be79c737a43793eae93f9627
Joaquín Balogna <30188421+longeill@users.noreply.github.com>
2023-07-16 15:22:58 -0400

    Update light-switch.js
    Wrote function in sublime and pasted here.

--- Projects/Light Switch/light-switch.js
+++ Projects/Light Switch/light-switch.js
@@ -1,7 +1,15 @@
-	function lightSwitch{
-		if (document.getElementById('bulb').src == 'pic_bulbon.gif') {
-	      document.getElementById("bulb").src = "pic_bulboff.gif";
-	  	} else {
-	      document.getElementById("bulb").src = "pic_bulbon.gif";
-		}
+let isBulbOn == false;
+let lightBulb == document.getElementById("bulb");
+let switchButton == document.getElementById("light-switch");
+
+function lightSwitch(){
+	if (isBulbOn){
+		lightBulb.src = "pic_bulboff.gif";
+		switchButton.innerHTML = "Turn Light On";
+		isBulbOn = false;
+	} else {
+		lightBulb.src = "pic_bulbon.gif";
+		switchButton.innerHTML = "Turn Light Off";
+		isBulbOn = true;
 	}
+}
