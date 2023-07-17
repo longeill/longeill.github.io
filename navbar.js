b9a5a96d91b5c80c5cb6c342de0b27fd8e4c7e51
Joaquín Balogna <30188421+longeill@users.noreply.github.com>
2023-07-17 03:08:54 -0400

    Update navbar.js
    Including projects.json

--- navbar.js
+++ navbar.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
               <a class="nav-link" href="resume.html">Resumé</a>
             </li>
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="projectsDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <a class="nav-link dropdown-toggle" href="projects.html" id="projectsDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Projects
               </a>
               <ul class="dropdown-menu" aria-labelledby="projectsDropdown">
@@ -39,31 +39,21 @@ document.addEventListener("DOMContentLoaded", function () {
   placeholder.innerHTML = navbarHtml;
 
   // Dynamically generate project links for the dropdown
-  var projectsDropdown = document.getElementById("projectsDropdown");
-  var projectsMenu = projectsDropdown.nextElementSibling;
-  var projectsDirectory = "Projects/";
+  var projectsMenu = document.querySelector("#projectsDropdown + .dropdown-menu");
+  var projectsJsonPath = "projects.json";
 
-  fetch(projectsDirectory)
-    .then((response) => response.text())
-    .then((html) => {
-      var parser = new DOMParser();
-      var doc = parser.parseFromString(html, "text/html");
-      var links = doc.querySelectorAll("a");
+  fetch(projectsJsonPath)
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach(function (project) {
+        var menuItem = document.createElement("li");
+        var linkItem = document.createElement("a");
+        linkItem.className = "dropdown-item";
+        linkItem.textContent = project.name;
+        linkItem.href = project.link;
 
-      links.forEach(function (link) {
-        var folderName = link.href.substring(link.href.lastIndexOf("/") + 1, link.href.length - 1);
-        var fileName = folderName + ".html";
-
-        if (fileName !== "html.html") {
-          var menuItem = document.createElement("li");
-          var linkItem = document.createElement("a");
-          linkItem.className = "dropdown-item";
-          linkItem.textContent = folderName;
-          linkItem.href = projectsDirectory + folderName + "/" + fileName;
-
-          menuItem.appendChild(linkItem);
-          projectsMenu.appendChild(menuItem);
-        }
+        menuItem.appendChild(linkItem);
+        projectsMenu.appendChild(menuItem);
       });
     });
 });
