fe547f29eafd8ede9a4f41f4b75583eda771f2f9
Joaquín Balogna <30188421+longeill@users.noreply.github.com>
2023-07-17 19:28:56 +0000

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -7,27 +7,30 @@
 // jk. It didn't work on anything in the projects/ directory at all. Every time I tried to iterate on this with GPT it made it worse.
 // Destroy, Rebuild.
 
+// Ok, in fairness, this was much more complicated when I first generated it. I think I could have made this by myself but got
+// lost in the sauce trying to make something fancy work.
+
 document.addEventListener("DOMContentLoaded", function () {
   var navbarHtml = `
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container">
-        <a class="navbar-brand" href="../index.html">Longeill</a>
+        <a class="navbar-brand" href="../../index.html">Longeill</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link" href="../index.html">Home</a>
+              <a class="nav-link" href="../../index.html">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="../workflow.html">Workflow</a>
+              <a class="nav-link" href="../../workflow.html">Workflow</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="../resume.html">Resumé</a>
+              <a class="nav-link" href="../../resume.html">Resumé</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="../projects.html">Projects</a>
+              <a class="nav-link" href="../../projects.html">Projects</a>
             </li>
           </ul>
         </div>
