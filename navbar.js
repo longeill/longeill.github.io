1851fb928b7377eac6908cc1774d61d6b574629c
Joaquín Balogna <30188421+longeill@users.noreply.github.com>
2023-07-17 19:24:15 +0000

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -11,23 +11,23 @@ document.addEventListener("DOMContentLoaded", function () {
   var navbarHtml = `
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container">
-        <a class="navbar-brand" href="index.html">Longeill</a>
+        <a class="navbar-brand" href="../index.html">Longeill</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link" href="index.html">Home</a>
+              <a class="nav-link" href="../index.html">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="workflow.html">Workflow</a>
+              <a class="nav-link" href="../workflow.html">Workflow</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="resume.html">Resumé</a>
+              <a class="nav-link" href="../resume.html">Resumé</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="projects.html">Projects</a>
+              <a class="nav-link" href="../projects.html">Projects</a>
             </li>
           </ul>
         </div>
