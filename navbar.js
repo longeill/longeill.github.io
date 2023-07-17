a4d84dca605d036091de32106103356c01dd6dbd
Joaquín Balogna <30188421+longeill@users.noreply.github.com>
2023-07-17 02:51:26 -0400

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,52 +1,67 @@
-function generateNavbar() {
-  var navbar = document.createElement("nav");
-  navbar.id = "main-nav";
-  navbar.className = "navbar navbar-expand-sm navbar-dark";
-
-  navbar.innerHTML = `
-    <a class="navbar-brand pl-3" href="#">&nbsp;Longeill</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-item nav-link" href="index.html">Home</a>
-        <a class="nav-item nav-link" href="workflow.html">Workflow</a>
-        <a class="nav-item nav-link" href="resume.html">Resume</a>
-        <div class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="projectsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Projects</a>
-          <div class="dropdown-menu" aria-labelledby="projectsDropdown">
-          </div>
+document.addEventListener("DOMContentLoaded", function () {
+  // Create the navbar HTML
+  var navbarHtml = `
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <a class="navbar-brand" href="index.html">Longeill</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="index.html">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="workflow.html">Workflow</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="resume.html">Resumé</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="projectsDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Projects
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="projectsDropdown">
+                <!-- Dynamically generated project links will be added here -->
+              </ul>
+            </li>
+          </ul>
         </div>
       </div>
-    </div>
+    </nav>
   `;
 
-  var dropdownMenu = navbar.querySelector(".dropdown-menu");
+  // Get the placeholder element
+  var placeholder = document.getElementById("navbar-placeholder");
 
-  fetch("https://api.github.com/repos/longeill/longeill.github.io/contents/projects")
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      data.forEach(function(item) {
-        var pageName = item.name.split(".")[0];
-        var pageLink = document.createElement("a");
-        pageLink.className = "dropdown-item";
-        pageLink.href = "projects/" + item.name;
-        pageLink.textContent = pageName;
-        dropdownMenu.appendChild(pageLink);
+  // Set the navbar HTML as the content of the placeholder
+  placeholder.innerHTML = navbarHtml;
+
+  // Dynamically generate project links for the dropdown
+  var projectsDropdown = document.getElementById("projectsDropdown");
+  var projectsMenu = projectsDropdown.nextElementSibling;
+  var projectsDirectory = "Projects/";
+
+  fetch(projectsDirectory)
+    .then((response) => response.text())
+    .then((html) => {
+      var parser = new DOMParser();
+      var doc = parser.parseFromString(html, "text/html");
+      var links = doc.querySelectorAll("a");
+
+      links.forEach(function (link) {
+        var folderName = link.href.substring(link.href.lastIndexOf("/") + 1, link.href.length - 1);
+        var fileName = folderName + ".html";
+
+        var menuItem = document.createElement("li");
+        var linkItem = document.createElement("a");
+        linkItem.className = "dropdown-item";
+        linkItem.textContent = folderName;
+        linkItem.href = projectsDirectory + folderName + "/" + fileName;
+
+        menuItem.appendChild(linkItem);
+        projectsMenu.appendChild(menuItem);
       });
-    })
-    .catch(function(error) {
-      console.log("Error fetching projects:", error);
     });
-
-  return navbar;
-}
-
-window.addEventListener("DOMContentLoaded", function() {
-  var navbarPlaceholder = document.getElementById("navbar-placeholder");
-  var navbar = generateNavbar();
-  navbarPlaceholder.parentNode.replaceChild(navbar, navbarPlaceholder);
 });
